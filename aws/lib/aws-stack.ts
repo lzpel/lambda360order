44faf72ae7bb1005e794dc7caaa8f769ec90591f
lzpel <18492524+lzpel@users.noreply.github.com>
2026-02-15 00:33:31 +0900

    aws deployを確認

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -10,22 +11,22 @@ export class AwsStack extends cdk.Stack {
 
     // Lambda Function (Docker Image from ../api directory)
     // Uses AWS Lambda Web Adapter inside the image
-    const apiFunction = new lambda.DockerImageFunction(this, 'ApiFunction', {
-      code: lambda.DockerImageCode.fromImageAsset('../api'),
-      architecture: lambda.Architecture.X86_64,
-      timeout: cdk.Duration.seconds(30),
-      memorySize: 256,
-    });
-
-    // Function URL
-    const functionUrl = apiFunction.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-      cors: {
-        allowedMethods: [lambda.HttpMethod.ALL],
-        allowedOrigins: ['*'],
-        allowedHeaders: ['*'],
+    const { lambda: apiFunction, lambda_url: functionUrl } = docker_image_function(
+      this,
+      "ApiFunction",
+      path.join(__dirname, "..", "..", "api"),
+      {
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 256,
+      },
+      {
+        cors: {
+          allowedMethods: [lambda.HttpMethod.ALL],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        },
       }
-    });
+    );
 
     // CloudFront Distribution
     const distribution = new cloudfront.Distribution(this, 'ApiDistribution', {
@@ -41,4 +42,23 @@ export class AwsStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'FunctionUrl', { value: functionUrl.url });
     new cdk.CfnOutput(this, 'DistributionDomainName', { value: distribution.domainName });
   }
+}
+
+const docker_image_function = (
+  construct: Construct,
+  id: string,
+  directory: string,
+  props_lambda?: Omit<cdk.aws_lambda.DockerImageFunctionProps, "code">,
+  props_url?: Omit<cdk.aws_lambda.FunctionUrlOptions, "authType">
+) => {
+  //https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda.DockerImageFunction.html
+  const lambda = new cdk.aws_lambda.DockerImageFunction(construct, id, {
+    code: cdk.aws_lambda.DockerImageCode.fromImageAsset(directory,),
+    ...props_lambda
+  });
+  const lambda_url = lambda.addFunctionUrl({
+    authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+    ...props_url
+  })
+  return { lambda, lambda_url }
 }
