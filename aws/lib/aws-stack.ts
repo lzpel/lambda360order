8bfbf0d3deb79cecdc41e250522f5ba8a23d9541
lzpel <18492524+lzpel@users.noreply.github.com>
2026-02-16 22:42:53 +0900

    s3デプロイ

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -4,61 +4,80 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class AwsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // Lambda Function (Docker Image from ../api directory)
-    // Uses AWS Lambda Web Adapter inside the image
-    const { lambda: apiFunction, lambda_url: functionUrl } = docker_image_function(
-      this,
-      "ApiFunction",
-      path.join(__dirname, "..", "..", "api"),
-      {
-        timeout: cdk.Duration.seconds(30),
-        memorySize: 256,
-      },
-      {
-        cors: {
-          allowedMethods: [lambda.HttpMethod.ALL],
-          allowedOrigins: ["*"],
-          allowedHeaders: ["*"],
-        },
-      }
-    );
+		// Lambda関数（../api ディレクトリのDockerイメージ使うで）
+		// イメージの中で AWS Lambda Web Adapter 使ってるわ
+		const { lambda: apiFunction, lambda_url: functionUrl } = docker_image_function(
+			this,
+			"ApiFunction",
+			path.join(__dirname, "..", "..", "api"),
+			{
+				timeout: cdk.Duration.seconds(30),
+				memorySize: 256,
+			},
+			{
+				cors: {
+					allowedMethods: [lambda.HttpMethod.ALL],
+					allowedOrigins: ["*"],
+					allowedHeaders: ["*"],
+				},
+			}
+		);
 
-    // CloudFront Distribution
-    const distribution = new cloudfront.Distribution(this, 'ApiDistribution', {
-      defaultBehavior: {
-        origin: new origins.FunctionUrlOrigin(functionUrl),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Disable caching for dynamic API
-        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
-      },
-    });
+		// S3バケットや
+		const bucket_step = new s3.Bucket(this, 'step', {
+			cors: [{
+				allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT, s3.HttpMethods.POST, s3.HttpMethods.HEAD],
+				allowedOrigins: ['*'],
+				allowedHeaders: ['*'],
+			}],
+		});
+		const bucket_memo = new s3.Bucket(this, 'memo');
 
-    new cdk.CfnOutput(this, 'FunctionUrl', { value: functionUrl.url });
-    new cdk.CfnOutput(this, 'DistributionDomainName', { value: distribution.domainName });
-  }
+		// Lambdaにアクセス権限あげるで
+		bucket_step.grantReadWrite(apiFunction);
+		bucket_memo.grantReadWrite(apiFunction);
+
+		// Lambdaに環境変数渡すで
+		apiFunction.addEnvironment('BUCKET_STEP', bucket_step.bucketName);
+		apiFunction.addEnvironment('BUCKET_MEMO', bucket_memo.bucketName);
+
+		// CloudFrontディストリビューションや
+		const distribution = new cloudfront.Distribution(this, 'ApiDistribution', {
+			defaultBehavior: {
+				origin: new origins.FunctionUrlOrigin(functionUrl),
+				viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+				allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+				cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // 動的APIやからキャッシュは無効にしとくで
+				originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+			},
+		});
+
+		new cdk.CfnOutput(this, 'FunctionUrl', { value: functionUrl.url });
+		new cdk.CfnOutput(this, 'DistributionDomainName', { value: distribution.domainName });
+	}
 }
 
 const docker_image_function = (
-  construct: Construct,
-  id: string,
-  directory: string,
-  props_lambda?: Omit<cdk.aws_lambda.DockerImageFunctionProps, "code">,
-  props_url?: Omit<cdk.aws_lambda.FunctionUrlOptions, "authType">
+	construct: Construct,
+	id: string,
+	directory: string,
+	props_lambda?: Omit<cdk.aws_lambda.DockerImageFunctionProps, "code">,
+	props_url?: Omit<cdk.aws_lambda.FunctionUrlOptions, "authType">
 ) => {
-  //https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda.DockerImageFunction.html
-  const lambda = new cdk.aws_lambda.DockerImageFunction(construct, id, {
-    code: cdk.aws_lambda.DockerImageCode.fromImageAsset(directory,),
-    ...props_lambda
-  });
-  const lambda_url = lambda.addFunctionUrl({
-    authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
-    ...props_url
-  })
-  return { lambda, lambda_url }
+	//https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda.DockerImageFunction.html
+	const lambda = new cdk.aws_lambda.DockerImageFunction(construct, id, {
+		code: cdk.aws_lambda.DockerImageCode.fromImageAsset(directory,),
+		...props_lambda
+	});
+	const lambda_url = lambda.addFunctionUrl({
+		authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+		...props_url
+	})
+	return { lambda, lambda_url }
 }
