129c8eba617a1aa38ab7e6d2f90f91d6141a1dfe
lzpel <18492524+lzpel@users.noreply.github.com>
2026-03-09 19:51:49 +0900

    update

--- demos/01_板金制御盤ボックス/script.js
+++ demos/01_板金制御盤ボックス/script.js
@@ -1,30 +1,22 @@
 
 // 1. Schema
 const params = {
-    width:  { type: "number", label: "幅",    unit: "mm", default: 300, constraint: { min: 200, max: 600, step: 10 } },
-    depth:  { type: "number", label: "奥行き", unit: "mm", default: 400, constraint: { min: 200, max: 800, step: 10 } },
-    height: { type: "number", label: "高さ",   unit: "mm", default: 150, constraint: { enum: [100, 150, 200] } },
-    color:  { type: "color",  label: "色",     default: "#cccccc", constraint: { enum: ["#cccccc", "#336699", "#993333"] } }
+	width: { type: "number", label: "幅", unit: "mm", default: 300, constraint: { min: 200, max: 600, step: 10 } },
+	depth: { type: "number", label: "奥行き", unit: "mm", default: 400, constraint: { min: 200, max: 800, step: 10 } },
+	height: { type: "number", label: "高さ", unit: "mm", default: 150, constraint: { enum: [100, 150, 200] } },
+	color: { type: "color", label: "色", default: "#cccccc", constraint: { enum: ["#cccccc", "#336699", "#993333"] } }
 };
 
 // 2. Shape Generator
 const lambda = (params) => {
-    return {
-    shape: {
-        op: "subtract",
-        a: {
-            op: "stretch",
-            shape: { op: "step", path: "box-base.step" },
-            cut: [100, 100, 75],
-            delta: [params.width - 200, params.depth - 200, params.height - 150]
-        },
-        b: {
-        op: "translate",
-        shape: { op: "step", path: "mounting-hole.step" },
-        xyz: [params.width * 0.5, params.depth - 20, 0]
-        }
-    },
-    color: params.color,
-    price: 5000 + (params.width * 10)
-    };
+	return {
+		shape: {
+			op: "stretch",
+			shape: { op: "step", content_hash: "c648cc3352136f4db46d0cbe133b6cf5bf9d208889186d0138ad9d671da72484" },
+			cut: [100, 100, 75],
+			delta: [params.width - 200, params.depth - 200, params.height - 150]
+		},
+		color: params.color,
+		price: 5000 + (params.width * 10)
+	};
 };
